db89a909972f0cfe6aa8f8ee7d081bf75d7261ab
Zelinsky Dmitry <zelinsky.mail@gmail.com>
2019-02-26 15:06:11 +0200

    подключены css файлы

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,25 +1,25 @@
 import React from 'react';
+
 import TodoListItem from './todo-list-item';
+import './todo-list.css';
 
 const TodoList = ({ todos }) => {
 
-    const elements = todos.map((item) => {
+  const elements = todos.map((item) => {
+    const { id, ...itemProps } = item;
 
-        const { id, ...itemProps } = item;
-
-        return(
-            // <li><TodoListItem label={item.label} important={item.important}/></li>
-            <li key={id}>
-                <TodoListItem { ...itemProps }/>
-            </li>
-        );
-    });
-
-    return(
-        <ul>
-            { elements }
-        </ul>
+    return (
+      <li key={id} className="list-group-item">
+        <TodoListItem {...itemProps } />
+      </li>
     );
-}
+  });
+
+  return (
+    <ul className="list-group todo-list">
+      { elements }
+    </ul>
+  );
+};
 
 export default TodoList;
