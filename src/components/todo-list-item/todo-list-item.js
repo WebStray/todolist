f1637393efc3851b80bb18cb8627f0f88ec1f7b4
Zelinsky <zelinsky.mail@gmail.com>
2019-03-07 21:07:25 +0200

    Логика done и important вынесена в компонент App. Перезаписывается лишь некоторые поля в state.

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,29 +4,13 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
-  state = {
-    done: false,
-    important: false
-  }
-
-  onLabelClick = () => {
-    this.setState({
-      done: true
-    });
-    // console.log(`click on ${this.props.label}`);
-  }
-
-  onMarkImportant = () => {
-    this.setState({
-      important: true
-    });
-  }
-
-
-
   render() {
-    const { label } = this.props;
-    const { done, important } = this.state;
+    const { label,
+            onDeleted,
+            onToggleImportant,
+            onToggleDone,
+            done,
+            important } = this.props;
 
     let classNames = 'todo-list-item';
     if( done ) {
@@ -41,18 +25,19 @@ export default class TodoListItem extends Component {
       <span className={classNames}>
         <span
           className="todo-list-item-label"
-          onClick={ this.onLabelClick } >
+          onClick={ onToggleDone } >
           {label}
         </span>
 
         <button type="button"
                 className="btn btn-outline-success btn-sm float-right"
-                onClick={ this.onMarkImportant }>
+                onClick={ onToggleImportant }>
           <i className="fa fa-exclamation" />
         </button>
 
         <button type="button"
-                className="btn btn-outline-danger btn-sm float-right">
+                className="btn btn-outline-danger btn-sm float-right"
+                onClick={onDeleted}>
           <i className="fa fa-trash-o" />
         </button>
       </span>
